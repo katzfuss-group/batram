8bc00a3bf06c2bcd8d2380def641b0fa115dfaaf
Anirban Chakraborty <anirban0451@gmail.com>
2024-05-14 10:48:50 -0500

    Custom Matern Kernel for using in ShrinkTM

--- src/batram/helpers.py
+++ src/batram/helpers.py
@@ -1,6 +1,9 @@
 from scipy import linalg, stats
+from scipy.spatial.distance import cdist, pdist, squareform
+from scipy.special import gamma, kv
 from sklearn.gaussian_process import kernels
 import numpy as np
+import torch
 
 
 def make_grid(nlocs: int, ndims: int) -> np.ndarray:
@@ -52,3 +55,37 @@ class GaussianProcessGenerator:
         chol = linalg.cholesky(covar, lower=True)
         z = stats.multivariate_normal(cov=np.eye(chol.shape[0]), seed=np.random.seed(seed)).rvs(num_reps)
         return np.dot(chol, z.T).T
+    
+
+###################### NOT IN USE YET (ANIRBAN, MAY 10, 2024) ###################################
+class CustomMaternKernel(torch.nn.Module):
+    """Initializes a Matern Kernel."""
+    def __init__(self, **kwargs):
+        super().__init__()
+        fix_nu = kwargs.get("fix_nu", 1.5)
+        self.nu = fix_nu
+        
+    
+    def forward(self, length_scale: torch.Tensor, locs_X:torch.Tensor, locs_Y: torch.Tensor | None):
+
+        if locs_Y is None:
+            distmatrix = torch.pdist(locs_X)
+        else:
+            assert locs_X.shape[1] == locs_Y.shape[1]
+            distmatrix = torch.cdist(locs_X, locs_Y)
+        distmatrix =  distmatrix.div(length_scale)
+
+        ## this part is mostly copied from sklearn.gaussian_process.kernels.py
+        if (self.nu == 0.5):
+            K = torch.exp(-distmatrix)
+        elif (self.nu == 1.5):
+            K = distmatrix.mul((torch.Tensor([3])).sqrt())
+            K = (1.0 + K) * torch.exp(-K)
+        elif (self.nu == 2.5):
+            K = distmatrix.mul((torch.Tensor([5])).sqrt())
+            K = (1.0 + K + (K**2).div(torch.Tensor([3.0]))) * torch.exp(-K)
+        elif self.nu == torch.inf:
+            K = torch.exp(-(distmatrix**2).div(torch.Tensor([2.0])))
+        return K
+        
+
